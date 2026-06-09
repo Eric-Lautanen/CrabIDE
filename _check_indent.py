1599ae3ca1076f14c78e40fba0dd02737757881c
Eric Lautanen <ericlautanen77@gmail.com>
2026-06-08 20:16:22 -0400

    chore: remove temp debug scripts

--- _check_indent.py
+++
@@ -1,8 +0,0 @@
-with open('crates/crabide-git/src/lib.rs', 'r') as f:
-    lines = f.readlines()
-# Line 519 (0-indexed: 518)
-print(repr(lines[518]))
-# Line 868 (0-indexed: 867)
-print(repr(lines[867]))
-# Line 869 (0-indexed: 868)
-print(repr(lines[868]))
--- _check_lines2.py
+++
@@ -1,5 +0,0 @@
-with open('crates/crabide-git/src/lib.rs', 'r') as f:
-    lines = f.readlines()
-for i in range(865, 876):
-    if i <= len(lines):
-        print(f'{i:4d}: {repr(lines[i-1])}')
